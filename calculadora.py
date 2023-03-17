3f97f79f250749a6231b3b4087d3411104f292c4
Berriondo <juan.gomezio@amigo.edu.co>
2023-03-17 11:30:10 -0500

    Finally Commit

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,12 @@
+from tkinter import *
+from tkinter import ttk
 import tkinter
 
 ventana = tkinter.Tk()
 ventana.title("Calculadora")
 ventana.geometry("+600+200")
 ventana.resizable(width=10, height=10)
+
 #Funciones de las operaciones matematicas
 def suma():
     label_result
@@ -89,6 +92,26 @@ def modulo():
         else:
             numero[2] = int(numero[0]) % int(numero[1])
             label_result["text"] = f"El resultado del modulo es {numero[2]}"      
+def operacion():
+    oper = combo.get()
+    if oper == "Suma":
+        suma()
+    elif oper =="Resta":
+        resta()
+    elif oper =="Multiplicacion":
+        multi()
+    elif oper =="Division":
+        div()
+    elif oper =="Exponente":
+        expo()
+    elif oper =="Modulo":
+        modulo()
+def limpiar():
+    combo.set("")
+    label_result["text"] = "Se han Borrado todos los campos"      
+    n1.delete(0,END)
+    n2.delete(0,END)
+
 
 #Labels o texto de la calculadora
 label_calculadora = tkinter.Label(ventana, text="Calculadora", font="courier 15")
@@ -96,19 +119,20 @@ label_n1 = tkinter.Label(ventana, text="Ingrese el primer valor",font="courier 1
 label_n2 = tkinter.Label(ventana, text="Ingrese el segundo valor", font="courier 10")
 label_result = tkinter.Label(ventana, font="courier 10", pady=10)
 
-
 #Entradas o textbox
 n1 = tkinter.Entry(ventana)
 n2 = tkinter.Entry(ventana)
 
 
 #Botones de las operaciones
-btn_sum = tkinter.Button(ventana, text="Suma", width=20, command=suma)
-btn_rest = tkinter.Button(ventana, text="Resta", width=20, command=resta)
-btn_mltp = tkinter.Button(ventana, text="Multiplicacion", width=20, command=multi)
-btn_div = tkinter.Button(ventana, text="Division", width=20, command=div)
-btn_expo = tkinter.Button(ventana, text="Exponente", width=20, command=expo)
-btn_modulo = tkinter.Button(ventana, text="Modulo", width=20, command=modulo)
+combo = ttk.Combobox(ventana,
+    state="readonly",
+    values=["Suma", "Resta", "Multiplicacion", "Division", "Exponente", "Modulo"]
+)
+
+oper = combo.current()
+btn_result = tkinter.Button(ventana, text="Calcular", command=operacion)
+btn_limpiar = tkinter.Button(ventana, text="Limpiar", command=limpiar)
 
 #Grid Metodo para manejar los espacios de la ventana como una matriz
 label_calculadora.grid(row=0, column=0, columnspan=2, pady=10)
@@ -116,12 +140,11 @@ label_n1.grid(row=1, column=0, pady=5)
 label_n2.grid(row=2, column=0, pady=5)
 n1.grid(row=1, column=1, pady=5, padx=5)
 n2.grid(row=2, column=1, pady=5, padx=5)
-btn_sum.grid(row=3, column=0, pady=10)
-btn_rest.grid(row=3, column=1, padx=10)
-btn_mltp.grid(row=4, column=1)
-btn_div.grid(row=4, column=0, pady=10)
-btn_expo.grid(row=5, column=0, pady=10)
-btn_modulo.grid(row=5, column=1, pady=10)
+btn_result.grid(row=3, column=1, pady=10)
+btn_limpiar.grid(row=3, column=2, pady=10)
+
+combo.grid(row=3, column=0)
+
 
 
 ventana.mainloop()
